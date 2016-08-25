9d881639c173fb3ab9a4daf89852ff00826265f6
Mike Li <lidingshan@gmail.com>
2016-08-26 07:56:44 +1000

    implemented all rules

--- life_game.rb
+++ life_game.rb
@@ -2,8 +2,12 @@ class LifeGame
 
     attr_reader :cells
 
-    def initialize(creation)
-        @cells = Marshal.load(Marshal.dump(creation))
+    def initialize
+        # next_generation(creation)
+    end
+
+    def next_generation(creation)
+        next_gen_cells = Marshal.load(Marshal.dump(creation))
 
         creation.each_with_index { |row, row_index|
             row.each_with_index{ |cell, index| 
@@ -25,10 +29,16 @@ class LifeGame
 
                 live_neighbours_count += (row_index+1 < creation.size && index+1 < row.size ? creation[row_index+1][index+1] : 0)
 
-                if live_neighbours_count < 2
-                    @cells[row_index][index] = 0
+                if live_neighbours_count < 2 || live_neighbours_count > 3
+                    next_gen_cells[row_index][index] = 0
+                end
+
+                if live_neighbours_count == 3 
+                    next_gen_cells[row_index][index] = 1
                 end
             }
         }
+
+        @cells = Marshal.load(Marshal.dump(next_gen_cells))
     end
 end
--- spec/life_spec.rb
+++ spec/life_spec.rb
@@ -10,62 +10,101 @@ require './life_game'
 
 describe "Game of Life" do
 
+    before(:each) do
+        @game = LifeGame.new
+    end
+
     it 'should die for one live cell' do
         creation = [[1]]
-        game = LifeGame.new(creation)
+        @game.next_generation(creation)
 
-        expect(game.cells).to eq([[0]])
+        expect(@game.cells).to eq([[0]])
     end
 
     it 'should die when have one live neighbour' do
         creation = [[1, 1]]
-        game = LifeGame.new(creation)
-        expect(game.cells).to eq([[0, 0]])
+        @game.next_generation(creation)
+        expect(@game.cells).to eq([[0, 0]])
     end
 
     it 'should keep live when have two live neighbours on left and right' do
         creation = [[1, 1, 1]]
-        game = LifeGame.new(creation)
-        expect(game.cells).to eq([[0, 1, 0]])
+        @game.next_generation(creation)
+        expect(@game.cells).to eq([[0, 1, 0]])
     end
 
     it 'should die when have one live neighbour in three cells' do
         creation = [[1, 1, 0]]
-        game = LifeGame.new(creation)
-        expect(game.cells).to eq([[0, 0, 0]])
+        @game.next_generation(creation)
+        expect(@game.cells).to eq([[0, 0, 0]])
     end
 
     it 'should keep die when live neighbours are not 3' do
         creation = [[1, 0, 1]]
-        game = LifeGame.new(creation)
-        expect(game.cells).to eq([[0, 0, 0]])
+        @game.next_generation(creation)
+        expect(@game.cells).to eq([[0, 0, 0]])
     end
 
     it 'should keep live when have two live neighbours on top and bottom' do
         creation = [[1], [1], [1]]
-        game = LifeGame.new(creation)
-        expect(game.cells).to eq([[0],[1],[0]])
+        @game.next_generation(creation)
+        expect(@game.cells).to eq([[0],[1],[0]])
     end
 
-    it 'should keep live when have live neighbours at top right and top left' do
-        creation = [[1, 0, 1],
-                    [0, 1, 0]]
-        game = LifeGame.new(creation)
-        expect(game.cells).to eq(
-            [[0, 0, 0],
-             [0, 1, 0]])
+    it 'should die when only have one live neighbour at top right or bottom left' do
+        creation = [[0, 1],
+                    [1, 0]]
+        @game.next_generation(creation)
+        expect(@game.cells).to eq(
+            [[0, 0],
+             [0, 0]])
     end
 
-    it 'should keep live when have live neighbours at bottom right and bottom left' do
-        creation = [[0, 1, 0],
-                    [1, 0, 1]]
-        game = LifeGame.new(creation)
-        expect(game.cells).to eq(
-            [[0, 1, 0],
-             [0, 0, 0]])
+    it 'should die when only have live neighbour at top lef or bottom right' do
+        creation = [[1, 0],
+                    [0, 1]]
+        @game.next_generation(creation)
+        expect(@game.cells).to eq(
+            [[0, 0],
+             [0, 0]])
     end
 
-    xit 'should die when have more than three live neighbours' do
-        pending('to be implemented')
+    it 'should live when have just three live neighbours' do
+        creation = [
+            [1, 1],
+            [1, 1]
+        ]
+
+        @game.next_generation(creation)
+        expect(@game.cells).to eq([
+                [1, 1],
+                [1, 1]
+            ])
+    end
+
+    it 'should die when have more than three live neighbours' do
+        creation = [
+            [1, 1, 1],
+            [1, 1, 1]
+        ]
+
+        @game.next_generation(creation)
+        expect(@game.cells).to eq([
+                [1, 0, 1],
+                [1, 0, 1]
+            ])
+    end
+
+    it 'should resurrect if a dead cell have exactly three live neighbours' do
+        creation = [
+            [1, 0, 1],
+            [0, 1, 0]
+        ]
+
+        @game.next_generation(creation)
+        expect(@game.cells).to eq([
+                [0, 1, 0],
+                [0, 1, 0]
+            ])
     end
 end
